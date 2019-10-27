ca85fdcaf9f6db786963a1a06d9510b55139f665
BrewinBeerz <daniel.s.squitin@gmail.com>
2019-10-26 19:14:03 -0700

    Added react-photo-album

--- src/Arches.js
+++ src/Arches.js
@@ -1,12 +1,91 @@
 import React, { Component } from "react";
-import './Arches.css'
- 
+//import Carousel from "react-bootstrap/Carousel"
+import Gallery from "react-photo-gallery"
+import './Album.css'
+
+const photos = [
+  {
+    src: './arches_card.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: './canyonlands_card.jpg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: './grandcanyon_card.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: './grandcanyon_card.jpg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: './canyonlands_card.jpg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: './grandcanyon_card.jpg',
+    width: 4,
+    height: 3
+  }
+];
+
 class Arches extends Component {
+
   render() {
     return (
+      /*
       <div>
-        <h2>Arches</h2>
-        <p>Blah blah blah blah blah</p>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./arches_card.jpg"
+              alt="First slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Arches National Park</h3>
+              <p>Photo by Danny Squitin</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100 b-"
+              src="./canyonlands_card.jpg"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Canyonlands National Park</h3>
+              <p>Photo by Danny Squitin</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./grandcanyon_card.jpg"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Grand Canyon National Park</h3>
+              <p>Photo by Danny Squitin</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      */
+      <div>
+        <Gallery photos={photos} direction={"column"} />
       </div>
     );
   }
