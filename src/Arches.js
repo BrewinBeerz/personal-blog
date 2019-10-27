6893ff5b0005171d6261414af16a957b2a464556
BrewinBeerz <daniel.s.squitin@gmail.com>
2019-10-26 19:38:31 -0700

    Fixed album

--- src/Arches.js
+++ src/Arches.js
@@ -1,7 +1,16 @@
-import React, { Component } from "react";
-//import Carousel from "react-bootstrap/Carousel"
+import React, { Component, useState, useCallback } from "react";
+import { render } from "react-dom";
 import Gallery from "react-photo-gallery"
+import Carousel, { Modal, ModalGateway } from "react-images";
 import './Album.css'
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+
+const images = [
+  './arches_card.jpg',
+  './canyonlands_card.jpg',
+  './grandcanyon_card.jpg',
+];
 
 const photos = [
   {
@@ -37,55 +46,40 @@ const photos = [
 ];
 
 class Arches extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+  }
 
   render() {
+    const { photoIndex, isOpen } = this.state;
+
     return (
-      /*
       <div>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./arches_card.jpg"
-              alt="First slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Arches National Park</h3>
-              <p>Photo by Danny Squitin</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100 b-"
-              src="./canyonlands_card.jpg"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Canyonlands National Park</h3>
-              <p>Photo by Danny Squitin</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./grandcanyon_card.jpg"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Grand Canyon National Park</h3>
-              <p>Photo by Danny Squitin</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-      */
-      <div>
-        <Gallery photos={photos} direction={"column"} />
+        <h2>Arches National Park</h2>
+        <Gallery photos={photos} direction={"column"} onClick={() => this.setState({ isOpen: true })}/>
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length,
+              })
+            }
+          />
+        )}
       </div>
     );
   }
