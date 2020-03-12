f4acededd957287a535fa17968918c0d034adec5
BrewinBeerz <daniel.s.squitin@gmail.com>
2020-03-12 01:23:58 -0700

    pruned unnecessary components

--- personal-blog/src/Main.js
+++ personal-blog/src/Main.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from "react-bootstrap/Container";
 import { HashRouter } from "react-router-dom";
 import Router from "../src/components/router/Router";
 import Header from '../src/components/header/Header'
